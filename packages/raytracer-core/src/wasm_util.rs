acd7613abf2a5aced0882c3fa1be391e1c222d7e
Joe Quigley <quigley.joseph@outlook.com>
2023-01-28 14:07:21 -0800

    Lint wasm-util

--- packages/raytracer-core/src/wasm_util.rs
+++ packages/raytracer-core/src/wasm_util.rs
@@ -1,27 +1,28 @@
 //! Helpers specific to WASM platforms
-//! 
+//!
 //! The helpers in this file are intended to be called in JS, and are made
 //! available as globals on the WASM binary.
+use crate::image::buffer;
+use crate::render;
 use console_error_panic_hook;
 use console_log;
 use log::{info, Level};
 use std::panic;
 use wasm_bindgen::prelude::*;
-// use crate::image::ppm;
-use crate::render;
-use crate::image::buffer;
 
+#[allow(dead_code)]
 #[wasm_bindgen]
 pub fn init_debug_hooks() {
-    console_log::init_with_level(Level::Info);
     panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init_with_level(Level::Info)
+        .expect("Failed to set console hooks, logging may not work");
     info!("Debug hooks set.");
 }
 
+#[allow(dead_code)]
 #[wasm_bindgen]
 pub fn draw_scene() -> Vec<u8> {
     let test_image = render::render_helloworld();
     let result = buffer::convert::rgb_to_rgba(&test_image, 255);
-    // let result = ppm::make_image(&test_image.data, test_image.width, test_image.height);
     result.data
 }
