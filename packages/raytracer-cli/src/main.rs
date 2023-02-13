07626a5fd96b9c91ef36ee873a5540a5cf7b25e8
Joe Quigley <quigley.joseph@outlook.com>
2023-02-12 21:51:46 -0800

    Add CLI params
    this way I don't have to recompile when fiddling with bounce limits and thread count

--- packages/raytracer-cli/src/main.rs
+++ packages/raytracer-cli/src/main.rs
@@ -1,6 +1,14 @@
-use std::{sync::Arc, thread::JoinHandle, time::SystemTime};
+use std::{
+    fs::File,
+    io::{self, Write},
+    path::PathBuf,
+    sync::Arc,
+    thread::JoinHandle,
+    time::SystemTime,
+};
 
 use cgmath::{point3, vec3, Deg, InnerSpace};
+use clap::Parser;
 use log::{debug, info};
 use raytracer_core::{
     image::{
@@ -12,17 +20,41 @@ use raytracer_core::{
     scene,
 };
 
-fn main() {
-    pretty_env_logger::init();
-    const THREADS: usize = 8;
-    const WIDTH: usize = 1280;
-    // const WIDTH: usize = 720;
-    const HEIGHT: usize = 720;
-    // const HEIGHT: usize = 405;
-    const SAMPLES_PER_PIXEL: usize = 32;
-    const MAX_RAY_DEPTH: i64 = 8;
+#[derive(Parser)]
+#[command(version)]
+struct CliArguments {
+    /// The number of threads to spawn
+    #[arg(short, long, default_value_t = 1)]
+    threads: usize,
+    /// The width of the image to render
+    #[arg(short, long, default_value_t = 720)]
+    width: usize,
+    /// The height of the image to render
+    #[arg(short, long, default_value_t = 405)]
+    height: usize,
+    /// The number of sample rays cast per pixel
+    #[arg(short, long, default_value_t = 4)]
+    samples_per_pixel: usize,
+    /// The maximum number of ray bounces a sample ray can generate
+    #[arg(short, long, default_value_t = 4)]
+    max_ray_depth: usize,
+    /// The output to write the result to. If not specified, defaults to stdout
+    #[arg(short, long)]
+    output_file: Option<PathBuf>,
+}
 
-    debug!("Output dimensions: {} x {}", WIDTH, HEIGHT);
+fn main() -> io::Result<()> {
+    pretty_env_logger::init();
+    let CliArguments {
+        threads,
+        width,
+        height,
+        samples_per_pixel,
+        max_ray_depth,
+        output_file,
+    } = CliArguments::parse();
+
+    debug!("Output dimensions: {} x {}", width, height);
 
     info!("Rendering image...");
     let start = SystemTime::now();
@@ -31,7 +63,7 @@ fn main() {
 
     let scene = Arc::new(scene::new_random_world());
 
-    for chunk in ChunkedPixelIterator::with_chunks(WIDTH, HEIGHT, THREADS) {
+    for chunk in ChunkedPixelIterator::with_chunks(width, height, threads) {
         info!("Spawning thread...");
         // make a copy of the world specific to each thread
         // this helps the borrow checker see the move into the thread, without
@@ -44,15 +76,21 @@ fn main() {
                 camera_position,
                 look_at,
                 vec3(0.0, 1.0, 0.0),
-                WIDTH as f64 / HEIGHT as f64,
+                width as f64 / height as f64,
                 Deg(20.0),
                 22.0,
                 (look_at - camera_position).magnitude(),
                 0.0,
                 1.0,
             );
-            let renderer = Renderer::new(WIDTH, HEIGHT, SAMPLES_PER_PIXEL, MAX_RAY_DEPTH, camera);
-            let mut buf = ImageBuffer::new_rgb(WIDTH, HEIGHT);
+            let renderer = Renderer::new(
+                width,
+                height,
+                samples_per_pixel,
+                max_ray_depth as i64,
+                camera,
+            );
+            let mut buf = ImageBuffer::new_rgb(width, height);
             renderer.render_to_buffer(&local_scene, &mut buf, chunk);
             buf
         }));
@@ -68,10 +106,22 @@ fn main() {
     let result_image = blend::blend_images(images, BlendingMode::Add);
 
     let end = SystemTime::now();
-    let result = ppm::make_image(&result_image.data, result_image.width, result_image.height);
     info!(
         "Rendering took {} ms",
         end.duration_since(start).expect("you doltz").as_millis()
     );
-    println!("{}", result);
+
+    let result = ppm::make_image(&result_image.data, result_image.width, result_image.height);
+
+    if let Some(filepath) = output_file {
+        let mut file = File::create(filepath)?;
+        file.write(result.as_bytes())?;
+        file.flush()?;
+    } else {
+        let mut stdout = io::stdout();
+        stdout.write(result.as_bytes())?;
+        stdout.flush()?;
+    };
+
+    Ok(())
 }
